4d266aff1159838ebb52e8c551cbc7156e207e5e
Chi <s.waratnan@gmail.com>
2016-04-15 21:00:25 +0700

    Initial Commit

---
+++ ninjaname/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ninjaname
+//
+//  Created by Chi on 4/15/2559 BE.
+//  Copyright © 2559 Chi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
